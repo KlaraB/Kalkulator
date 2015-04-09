db1b314c310168ebbd23acce9e6381e0446f7466
Klara Bukovec <klara.bukovec@gmail.com>
2015-04-09 18:16:03 +0200

    polepsan kalkulator

--- kalkulator.py
+++ kalkulator.py
@@ -2,21 +2,19 @@ __author__ = 'U269074'
 
 print ("Welcome to Klara's calculator!")
 
-x = int(raw_input("Definiraj prvo stevilko"))
-y = float(raw_input("Definiraj drugo stevilko"))
+x = int(raw_input("Definiraj prvo stevilko: "))
+y = float(raw_input("Definiraj drugo stevilkor: "))
 
-z = raw_input("Izberi racunsko operacijo (sestevanje/odstevanje/mnozenje/deljenje/potenciranje")
+z = raw_input("Napisi racunsko operacijo (sestevanje/odstevanje/mnozenje/deljenje/potenciranje): ")
 
-if z =="sestevanje":
+if z == "sestevanje":
     b = x + y
-    print "sestevek je " + str(b)
-elif z=="odstevanje":
-    print x-y
-elif z=="mnozenje":
+    print "Rezultat je " + str(b)
+elif z == "odstevanje":
+    print x - y
+elif z == "mnozenje":
     print x * y
-elif z=="deljenje":
+elif z == "deljenje":
     print x/y
-elif z=="potenciranje":
+elif z == "potenciranje":
     print x**y
-else:
-     print("Slabe izbire")
